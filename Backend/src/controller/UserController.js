c7f966e3fb583bebf1e1935be9a5197fd7748ab0
RajanDhamala <Rajandhamala0123@gmail.com>
2025-09-01 21:34:57 +0545

    vercel realod and config issue

--- Backend/src/controller/UserController.js
+++ Backend/src/controller/UserController.js
@@ -75,10 +75,10 @@ res.cookie("refreshToken", newRefreshtoken, {
 });
 
 res.cookie("currentUser", JSON.stringify(currentUser), {
-  httpOnly: false,         // frontend JS can read it
+  httpOnly: false,  // frontend JS can read/write
   secure: true,
   sameSite: "none",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 });
 
